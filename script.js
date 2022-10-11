049d7cb6fc5fee3828c4739cb6d518018645ed7f
Shiva Prakash Pendem <pendemsivaprakash@gmail.com>
2022-10-11 15:22:31 +0100

    update mobile styles and add desktop styles

--- script.js
+++ script.js
@@ -1,34 +1,20 @@
 window.onload = () => {
   const iconShareContainer = document.querySelector("#share-icon-container");
+  const iconShareClickedContainer = document.querySelector(
+    "#share-icon-clicked-container"
+  );
+  const socialsContainer = document.querySelector("#socials-container");
 
-  iconShareContainer.addEventListener("click", () => {
-    const iconShare = document.querySelector("#share-icon");
-    displaySocials();
-
-    toggleClass("bg-blue-100", iconShareContainer);
-    toggleClass("bg-blue-300", iconShareContainer);
-    toggleClass("fill-blue-400", iconShare);
-    toggleClass("fill-white", iconShare);
-  });
-
-  function displaySocials() {
-    const socialsContainer = document.querySelector("#socials-container");
-    const socials = document.querySelector("#socials");
-    const profile = document.querySelector("#profile");
-    const profileContainer = document.querySelector("#profile-container");
-
-    toggleClass("justify-between", socialsContainer);
-    toggleClass("w-full", socialsContainer);
-
-    toggleClass("bg-blue-400", profileContainer);
-    toggleClass("py-[1.31rem]", profileContainer);
-
-    toggleClass("hidden", profile);
-
-    toggleClass("hidden", socials);
-  }
+  toggleContainerOnClick(socialsContainer, iconShareContainer);
+  toggleContainerOnClick(socialsContainer, iconShareClickedContainer);
 
   function toggleClass(className, element) {
     return element.classList.toggle(className);
+  }
+
+  function toggleContainerOnClick(container, clickableContainer) {
+    return clickableContainer.addEventListener("click", () => {
+      toggleClass("hidden", container);
+    });
   }
 };
